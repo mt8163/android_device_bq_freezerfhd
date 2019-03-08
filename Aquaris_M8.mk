eaf8f27ba3ae4d7eec23da3aca7fea9d1b192287
Rortiz2005 <39171306+Rortiz2005@users.noreply.github.com>
2019-03-08 08:44:13 +0100

    Update PRODUCT_NAME for official support.

--- Aquaris_M8.mk
+++ Aquaris_M8.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/malatamobile/bq_aquaris_m8/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Aquaris_M8
-PRODUCT_NAME := Aquaris_M8
+PRODUCT_NAME := karin
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris_M8
