a77f20b29b92714a1bc7d806e25660048df37bca
R0rtiz2 <39171306+R0rtiz2@users.noreply.github.com>
2019-04-16 23:23:25 +0200

    Update karin.mk

--- karin.mk
+++ karin.mk
@@ -2,9 +2,9 @@
 $(call inherit-product, device/malatamobile/bq_aquaris_m8/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Aquaris_M8
+PRODUCT_DEVICE := karin
 PRODUCT_NAME := karin
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
-PRODUCT_MODEL := Aquaris_M8
-TARGET_DEVICE = bq_aquaris_m8
+PRODUCT_MODEL := Aquaris M8
+TARGET_DEVICE = karin
