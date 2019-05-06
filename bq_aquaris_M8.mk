16fe07424d18e14081d2416d76a0e359a9f835c7
R0rtiz2 <39171306+R0rtiz2@users.noreply.github.com>
2019-05-06 15:16:16 +0200

    Change device names

--- bq_aquaris_M8.mk
+++ bq_aquaris_M8.mk
@@ -1,10 +1,10 @@
 # Inherit device configuration
-$(call inherit-product, device/bq/karin/device.mk)
+$(call inherit-product, device/bq/bq_aquaris_M8/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := karin
-PRODUCT_NAME := karin
+PRODUCT_DEVICE := bq_aquaris_M8
+PRODUCT_NAME := bq_aquaris_M8
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M8
-TARGET_DEVICE = karin
+TARGET_DEVICE = bq_aquaris_M8
