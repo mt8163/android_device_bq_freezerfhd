1f26e81d95db127613ba5c71231d7a6d5c5912a7
R0rtiz2 <39171306+R0rtiz2@users.noreply.github.com>
2019-05-14 21:45:58 +0200

    Remove OEM name

--- aquaris_M8.mk
+++ aquaris_M8.mk
@@ -1,10 +1,10 @@
 # Inherit device configuration
-$(call inherit-product, device/bq/bq_aquaris_M8/device.mk)
+$(call inherit-product, device/bq/aquaris_M8/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := bq_aquaris_M8
-PRODUCT_NAME := bq_aquaris_M8
+PRODUCT_DEVICE := aquaris_M8
+PRODUCT_NAME := aquaris_M8
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M8
-TARGET_DEVICE = bq_aquaris_M8
+TARGET_DEVICE = aquaris_M8
