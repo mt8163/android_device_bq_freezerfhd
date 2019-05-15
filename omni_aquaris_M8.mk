dcf5c619a0cf768d5c922b896455ed35ef1633a9
R0rtiz2 <39171306+R0rtiz2@users.noreply.github.com>
2019-05-15 12:06:13 +0200

    Update omni_aquaris_M8.mk

--- omni_aquaris_M8.mk
+++ omni_aquaris_M8.mk
@@ -2,9 +2,9 @@
 $(call inherit-product, device/bq/aquaris_M8/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := aquaris_M8
-PRODUCT_NAME := aquaris_M8
+PRODUCT_DEVICE := omni_quaris_M8
+PRODUCT_NAME := omni_aquaris_M8
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M8
-TARGET_DEVICE = aquaris_M8
+TARGET_DEVICE = omni_aquaris_M8
