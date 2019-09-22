8465078773266573d13a0901ebef7f01408f699f
R0rt1z2 <rogerortizleal@gmail.com>
2019-09-22 12:57:50 +0200

    Add one more shim for vold

--- libshims/vold.cpp
+++ libshims/vold.cpp
@@ -1,3 +1,4 @@
 extern "C" void _ZN10properties3refE(void) {}
 extern "C" void _ZN10properties10is_defaultE(void) {}
 extern "C" void _ZN10properties3keyE(void) {}
+extern "C" void _ZN10properties5propsE(void) {}
