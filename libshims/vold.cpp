464b6c9c16d6867b83cf2bb4288357599b92e00a
R0rt1z2 <rogerortizleal@gmail.com>
2019-09-22 13:07:49 +0200

    LOL-Vold is starting to be a bit crazy

--- libshims/vold.cpp
+++ libshims/vold.cpp
@@ -3,3 +3,4 @@ extern "C" void _ZN10properties10is_defaultE(void) {}
 extern "C" void _ZN10properties3keyE(void) {}
 extern "C" void _ZN10properties5propsE(void) {}
 extern "C" void _ZNK21UnencryptedProperties3GetINSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEEET_PKcS8_(void) {}
+extern "C" void _ZNK21UnencryptedProperties2OKEv(void) {}
