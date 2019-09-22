6ea3ef684df684a0ed85a1e2dab9dd4277cefefc
R0rt1z2 <rogerortizleal@gmail.com>
2019-09-22 13:02:20 +0200

    Another shim... Vold is starting to be a bit heavy...

--- libshims/vold.cpp
+++ libshims/vold.cpp
@@ -2,3 +2,4 @@ extern "C" void _ZN10properties3refE(void) {}
 extern "C" void _ZN10properties10is_defaultE(void) {}
 extern "C" void _ZN10properties3keyE(void) {}
 extern "C" void _ZN10properties5propsE(void) {}
+extern "C" void _ZNK21UnencryptedProperties3GetINSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEEET_PKcS8_(void) {}
