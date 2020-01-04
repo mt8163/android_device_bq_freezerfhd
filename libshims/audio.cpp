d8b8ed34cf2f89bfb12c24df95ec70d3cf221a7c
Roger Ortiz <rogerortizleal@gmail.com>
2020-01-04 01:52:29 +0100

    aquaris_m8: Remove unused shims

--- libshims/audio.cpp
+++
@@ -1,3 +0,0 @@
-extern "C" void _ZN7android11AudioSystem24getVoiceUnlockDLInstanceEv(void) {}
-extern "C" void _ZN7android11AudioSystem23GetVoiceUnlockDLLatencyEv(void) {}
-extern "C" void _ZN7android11AudioSystem17SetVoiceUnlockSRCEjj(void) {}
--- libshims/media.cpp
+++
@@ -1,2 +0,0 @@
-extern "C" void _ZN7android13MediaRecorder18setParametersExtraERKNS_7String8E(void) {}
-extern "C" void _ZN7android9MtpServer17sendObjectUpdatedEj(void) {}
