c8fe1f51b0fcadacf8ae03ce987be63a0f83659f
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-01-14 09:47:33 +0100

    Update karin_audio.cpp

--- libkarin/karin_audio.cpp
+++ libkarin/karin_audio.cpp
@@ -1,3 +1,7 @@
+#include <log/log.h>
+#define LOG_TAG "AUDIO_SHIM"
+
+ALOGI("libkarin: dlopen audio_shim");
 extern "C" void _ZN7android11AudioSystem24getVoiceUnlockDLInstanceEv(void) {}
 extern "C" void _ZN7android11AudioSystem23GetVoiceUnlockDLLatencyEv(void) {}
 extern "C" void _ZN7android11AudioSystem17SetVoiceUnlockSRCEjj(void) {}
