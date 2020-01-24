a7afefb1983e58f7754f7d07663b4d20d3353ee3
Roger Ortiz <rogerortizleal@gmail.com>
2020-01-24 23:47:56 +0100

    aquaris_m8: Call vendor_bq_mt8163

--- device.mk
+++ device.mk
@@ -181,6 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Call vendor blobs
 include vendor/bq/aquaris_m8/aquaris_m8-vendor.mk
+include vendor/bq/mt8163/mt8163-vendor.mk
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
