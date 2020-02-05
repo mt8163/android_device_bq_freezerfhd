dcbaa97f6c8c37000e4d862c5e76f89db80ab75e
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-05 21:32:36 +0100

    aquaris_m8: Cleanup

--- Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),aquaris_m8)
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
-
-endif
--- lineage.mk
+++
@@ -1,9 +0,0 @@
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-# Inherit device configuration for the BQ Aquaris M8
-$(call inherit-product, device/bq/aquaris_m8/lineage_aquaris_m8.mk)
-
-# Product defines
-PRODUCT_NAME := lineage_aquaris_m8
-PRODUCT_RELEASE_NAME := aquaris_m8
