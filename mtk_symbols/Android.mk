6791ae0d358f10d1ca3a6ee84e929b43b41d5f0e
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-07 15:06:22 +0100

    aquaris_m8: Remove icu/fonts related stuff.

--- mtk_symbols/Android.mk
+++ mtk_symbols/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    icu55.c \
     mtk_asc.cpp \
     mtk_audio.cpp \
     mtk_fence.cpp \
@@ -12,7 +11,7 @@ LOCAL_SRC_FILES := \
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder liblog libgui libui libicuuc libicui18n libmedia
+LOCAL_SHARED_LIBRARIES := libbinder liblog libgui libui libmedia
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
