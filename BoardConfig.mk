9fc8e1d47bbf35ab696868350948102a30e84977
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-07 17:07:16 +0100

    aquaris_m8: Enable MALLOC_SVELTE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,7 @@ WIFI_DRIVER_FW_PATH_STA:=P2P
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
+MALLOC_SVELTE := true
 
 # Charger
 BOARD_CHARGER_SHOW_PERCENTAGE := true
