5f74e811d93c716c45dfe62fbd195501d0444bb7
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-18 16:57:46 +0100

    aquaris_m8: libmtk_symbols: cleanup.
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- mtk_symbols/Android.mk
+++ mtk_symbols/Android.mk
@@ -10,21 +10,12 @@ LOCAL_SRC_FILES := \
     mtk_icu55.c \
     mtk_icu53.c
 
-# only for 64bit libraries
+# 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
+# 64bit libraries end
 
 LOCAL_SHARED_LIBRARIES := libbinder liblog libgui libui libmedia libbase libstagefright_foundation libicuuc libicui18n libcrypto
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-# for libshim_mtkcam
-# /system/lib/libcam_utils.so ( _ZN7android13GraphicBufferC1Ejjij )
-#include $(CLEAR_VARS)
-#LOCAL_LDLIBS := -llog
-#LOCAL_SRC_FILES := mtk_cam.cpp
-#LOCAL_MODULE := libshim_mtkcam
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_PROPRIETARY_MODULE := true
-#include $(BUILD_SHARED_LIBRARY)
