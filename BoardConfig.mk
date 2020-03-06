0a453514d0f772d5a64648d747efcc4a0b2b73c7
Roger Ortiz <rogerortizleal@gmail.com>
2020-03-06 21:19:07 +0100

    aquaris_m8: Revert all Wi-Fi crap.
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,15 +100,19 @@ BOARD_USES_MTK_AUDIO := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # WIFI
+BOARD_WLAN_DEVICE := MediaTek
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
-WIFI_DRIVER_FW_PATH_PARAM := "/dev/wmtWifi"
-WIFI_DRIVER_FW_PATH_STA:=STA
-WIFI_DRIVER_FW_PATH_AP:=AP
-WIFI_DRIVER_FW_PATH_P2P:=P2P
+WIFI_DRIVER_FW_PATH_PARAM := /dev/wmtWifi
+WIFI_DRIVER_FW_PATH_AP := AP
+WIFI_DRIVER_FW_PATH_STA := STA
+WIFI_DRIVER_FW_PATH_P2P := P2P
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
+WIFI_DRIVER_STATE_ON := 1
+WIFI_DRIVER_STATE_OFF := 0
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
@@ -136,6 +140,7 @@ LINKER_FORCED_SHIM_LIBS := \
 	/system/lib64/libui_ext.so|libshim_ui.so \
 	/system/lib/libgui_ext.so|libshim_ui.so \
 	/system/lib64/libgui_ext.so|libshim_ui.so
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.mt8163
 BOARD_HAS_NO_SELECT_BUTTON := true
