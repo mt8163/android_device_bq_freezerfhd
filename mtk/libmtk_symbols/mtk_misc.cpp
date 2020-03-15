e005637114f15b0195db69ddcb778e596140e6e8
Roger Ortiz <rogerortizleal@gmail.com>
2020-03-15 11:31:57 +0100

    aquaris_m8: libmtk_symbols: Add missing symbols of program_binary_service.
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- mtk/libmtk_symbols/mtk_misc.cpp
+++ mtk/libmtk_symbols/mtk_misc.cpp
@@ -14,5 +14,11 @@ extern "C" {
     {
         return 0x0;
     }
-    
+
+    /* android::IPprogramBinaryService::uiRendererProgramAtlasTerminate */
+    int _ZN7android10uirenderer12ProgramAtlas9terminateEv(void)
+    {
+        return 0x0;
+    }
+
 }
