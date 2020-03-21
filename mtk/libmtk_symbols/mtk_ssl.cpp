d3bb85cec0e2b7653de63602212fcc88025899ed
Roger Ortiz <rogerortizleal@gmail.com>
2020-03-21 23:57:43 +0100

    aquaris_m8: libmtk_symbols: Fix SSL shim.
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

--- mtk/libmtk_symbols/mtk_ssl.cpp
+++ mtk/libmtk_symbols/mtk_ssl.cpp
@@ -2,26 +2,31 @@
 
 #define LOG_TAG "SSL_ctrl_stub"
 #include <log/log.h>
+#include <string.h>
 
-extern "C" long SSL_ctrl(int *ssl, int cmd, long larg, void *parg) {
-    ALOGD("SSL_ctrl: ssl=%p cmd=%d larg=%ld parg=%p", ssl, cmd, larg, parg);
-    return -1;
-}
+extern "C"
+{
+    long SSL_ctrl(int *ssl, int cmd, long larg, void *parg) {
+        ALOGD("SSL_ctrl: ssl=%p cmd=%d larg=%ld parg=%p", ssl, cmd, larg, parg);
+        return -1;
+    }
 
-extern "C" long SSL_CTX_ctrl(int *ctx, int cmd, long larg, void *parg) {
-    ALOGD("SSL_CTX_ctrl: ctx=%p cmd=%d larg=%ld parg=%p", ctx, cmd, larg, parg);
-    return -1;
-}
+    long SSL_CTX_ctrl(int *ctx, int cmd, long larg, void *parg) {
+        ALOGD("SSL_CTX_ctrl: ctx=%p cmd=%d larg=%ld parg=%p", ctx, cmd, larg, parg);
+        return -1;
+    }
 
-extern "C" void CRYPTO_free(void *str) {
-    free(str)
-}
+    void CRYPTO_free(void *str) {
+        ALOGD("CRYPTO_free: str=%s", str);
+        free(str);
+    }
 
-extern "C" void *CRYPTO_malloc(int num, const char *file, int line) {
-    ALOGD("CRYPTO_malloc: num=%d file=%s line=%d", num, file, line);
-    return 0;
-}
+    int CRYPTO_malloc(int num, const char *file, int line) {
+        ALOGD("CRYPTO_malloc: num=%d file=%s line=%d", num, file, line);
+        return malloc(num);
+    }
 
-extern "C" void CRYPTO_lock(int mode, int type, const char *file, int line) {
-    ALOGD("CRYPTO_lock: mode=%d type=%d file=%s line=%d", mode, type, file, line);
+    void CRYPTO_lock(int mode, int type, const char *file, int line) {
+        ALOGD("CRYPTO_lock: mode=%d type=%d file=%s line=%d", mode, type, file, line);
+    }
 }
