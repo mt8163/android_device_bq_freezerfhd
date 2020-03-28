9d33b740f87083dca186baedda122e3b8582e8f1
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-03-28 23:57:39 +0100

    aquaris_m8: libmtk_symbols: Just return 0

--- mtk/libmtk_symbols/mtk_audio.cpp
+++ mtk/libmtk_symbols/mtk_audio.cpp
@@ -6,6 +6,8 @@
 #include <media/IAudioPolicyService.h>
 #include <system/audio.h>
 
+#define LOG_TAG "AUDIO_SHIM"
+
 extern "C" {
     bool _ZN7android11AudioSystem24getVoiceUnlockDLInstanceEv(){
         return 0;
@@ -27,15 +29,8 @@ extern "C" {
         return 0;
     }
     
-    status_t _ZN7android11AudioSystem12SetAudioDataEijPv(int par1, size_t byte_len, void *ptr) {
-         ALOGD("SetAudioData");
-         const sp<IAudioFlinger>& af = AudioSystem::get_audio_flinger();
-         if (af == 0)
-         {
-             ALOGE("AudioSystem::SetAAudioData Error!! PERMISSION_DENIED");
-            return PERMISSION_DENIED;
-         }
-         return af->SetAudioData(par1,byte_len,ptr);
+    int _ZN7android11AudioSystem12SetAudioDataEijPv(int a1) {
+        return 0;
     }
     
     int _ZN7android11AudioSystem20GetVoiceUnlockULTimeEPv(void* DLtime) {
