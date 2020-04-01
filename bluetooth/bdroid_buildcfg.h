ef914afc8e08a1854d343fd8a65c3ae3b373066c
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-01 21:36:26 +0200

    aquaris_m8: Cleanup for LineageOS 15.1
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BDROID_BUILDCFG_H
-#define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "Aquaris M8"
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-
-#endif
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-for var in userdebug; do
- add_lunch_combo lineage_aquaris_m8-$var
-done
