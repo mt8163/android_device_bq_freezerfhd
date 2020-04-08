ae757c9e86fea08e64d8fb71a260b23b885aeb73
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-08 12:56:24 +0200

    aquaris_m8: Update HIDL and manifest for bluetooth/camera.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -32,10 +32,15 @@ PRODUCT_PACKAGES += \
 # Camera HAL
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl-legacy
 
-# Bluetooth HAL (MTK)
+# Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl-mediatek \
-    android.hardware.bluetooth@1.0-service-mediatek
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
