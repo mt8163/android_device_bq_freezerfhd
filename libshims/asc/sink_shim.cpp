6736ab23e86f429eaaab0c95ec258dda84935641
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-09 16:00:42 +0200

    aquaris_m8: More shims!
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- libshims/asc/sink_shim.cpp
+++ libshims/asc/sink_shim.cpp
@@ -1,10 +1,21 @@
 /* Those symbols are requested by the "sink" binary located at "/system/bin/" 
-   Actually IDA didn't showed up any function regarding to those so just return 0 */
+   Actually IDA didn't showed up any function for some of to those so just return 0 */
 
 #include <inttypes.h>
+#include <sys/types.h>
 
 extern "C"
 {
+    int _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPii(
+		void*, void*, int, int*, pid_t);
+
+    int _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEEPKcbPii(
+		void *looper, void *mime, int encoder, int *err, pid_t pid)
+    {
+	    return _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPii(
+			looper, mime, encoder, err, pid);
+    }
+
     int _ZN7android10DataSource23RegisterDefaultSniffersEv()
     {
         return 0;
