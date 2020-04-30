26514276a8ea9a34528a9f5c82f0049a5c675117
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-30 14:58:06 +0200

    aquaris_m8: Fix build error & shim error.
    * CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEEPKcbPii" referenced by "/system/lib64/libsource.so"...
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- libshims/asc/MediaCodec_shim.cpp
+++ libshims/asc/MediaCodec_shim.cpp
@@ -9,14 +9,17 @@ using namespace android;
 
 extern "C"
 {
-    void _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPiij(const sp<ALooper> &looper, const AString &mime, bool encoder, status_t *err, pid_t pid, uid_t uid);
-
     void _ZNK7android10MediaCodec15getInputBuffersEPNS_6VectorINS_2spINS_16MediaCodecBufferEEEEE(Vector<sp<ABuffer> > *buffers); /* Vector<sp<MediaCodecBuffer> > *buffers */
     void _ZNK7android10MediaCodec16getOutputBuffersEPNS_6VectorINS_2spINS_16MediaCodecBufferEEEEE(Vector<sp<ABuffer> > *buffers); /* Vector<sp<MediaCodecBuffer> > *buffers */
 
-    void _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPii(const sp<ALooper> &looper, const AString &mime, bool encoder, status_t *err, pid_t pid)
+    int _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPii(
+		void*, void*, int, int*, pid_t);
+
+    int _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEEPKcbPii(
+		void *looper, void *mime, int encoder, int *err, pid_t pid)
     {
-	    _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPiij(looper, mime, encoder, err, pid, 0);
+	    return _ZN7android10MediaCodec12CreateByTypeERKNS_2spINS_7ALooperEEERKNS_7AStringEbPii(
+			looper, mime, encoder, err, pid);
     }
 
     void _ZNK7android10MediaCodec15getInputBuffersEPNS_6VectorINS_2spINS_7ABufferEEEEE(Vector<sp<ABuffer> > *buffers)
