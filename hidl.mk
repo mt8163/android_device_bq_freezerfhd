7b8bb0233ff08d8c76b91ab008a25f7361221c89
R0rt1z2 <rogerortizleal@gmail.com>
2020-05-19 19:53:27 +0200

    Revert "aquaris_m8: Bring up Bluetooth."
    This reverts commit 413af586e0635861177545f30e22b7c202f504ce.

--- hidl.mk
+++ hidl.mk
@@ -37,10 +37,10 @@ PRODUCT_PACKAGES += \
 # Camera HAL
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl-legacy
 
-# Bluetooth HAL
+# Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-impl-mediatek \
+    android.hardware.bluetooth@1.0-service-mediatek
 
 # Memtrack
 PRODUCT_PACKAGES += \
