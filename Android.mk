e02cbe27644666cf7d1e0bedd25642020c83fea2
Kai Jones <joneskai626@gmail.com>
2020-06-30 09:29:38 -0700

    aquaris_m8: symlink nvdata to /data/nvram.
    * this is for modern nvram libs.
    * this will be there in case if we update nvram blobs
    
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I6a0fa1dfcb6f24aeb5977870e20201a1cbdc4fda

--- Android.mk
+++ Android.mk
@@ -1,5 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 ifneq ($(filter aquaris_m8,$(TARGET_DEVICE)),)
-   include $(call all-makefiles-under,$(LOCAL_PATH))
-   include $(CLEAR_VARS)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+NVDATA_SYMLINK := $(TARGET_OUT_VENDOR)/nvdata
+$(NVDATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "fstab link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/nvram $@
+
+ALL_DEFAULT_INSTALLED_MODULES +=  \
+    $(NVDATA_SYMLINK)
+
 endif
